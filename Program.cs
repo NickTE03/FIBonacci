21fc41461245bb73b13b4acd422fd337d006dc2e
NickTE03 <68023365+NickTE03@users.noreply.github.com>
2021-04-09 00:03:52 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FIBonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0, n2 = 1, n3;
+
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            for(n3=2; n3 < 3000; ++n3)
+            {
+                n3 = n1 + n2;
+                Console.WriteLine(n3);
+                n1 = n2;
+                n2 = n3;
+            }
+            
+
+        }
+    }
+}
